88915918e69cf9094163b279c8d3b046e0fe2b06
L E M O N <ryme.care@hotmail.com>
2024-01-31 21:38:52 +0530

    updated

---
+++ boot.py
@@ -0,0 +1,34 @@
+import logging
+from pyrogram import Client, __version__
+from info import API_ID, API_HASH, SESSION
+from aiohttp import web
+from stream.serve import web_server
+
+class Bot(Client):
+
+    def __init__(self):
+        super().__init__(
+            name="autofilter",
+            api_id=API_ID,
+            api_hash=API_HASH,
+            session=SESSION,
+            workers=10,
+            plugins={"root": "plugins"},
+            sleep_threshold=5,
+        )
+
+    async def start(self):
+        await super().start()
+        logging.info(f"Bot started")
+        #web-server
+        app = web.AppRunner(await web_server())
+        await app.setup()
+        port = "8080"
+        await web.TCPSite(app, "0.0.0.0", port).start()
+
+    async def stop(self, *args):
+        await super().stop()
+        logging.info("Bot stopped.")
+    
+app = Bot()
+app.run()
