276ca07ac8fa17e0a1e25f63ccf4aa27787b8a5f
L E M O N <ryme.care@hotmail.com>
2024-01-31 23:45:51 +0530

    updated

---
+++ plugins/spam.py
@@ -0,0 +1,14 @@
+from pyrogram import Client, filters
+
+@Client.on_message(filters.command("spam", prefixes=".") & filters.me)
+async def spam(client, message):
+    _, *text_parts = message.text.split()
+    try:
+        number_of_messages = int(text_parts[0])
+        text = ' '.join(text_parts[1:])
+    except ValueError:
+        number_of_messages = 10
+        text = ' '.join(text_parts)
+    if text:  # Only send messages if there's text to be spammed
+        for _ in range(number_of_messages):
+            await message.reply_text(text)
