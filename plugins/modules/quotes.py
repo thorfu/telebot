480332cc32d735d99421e2ac730e4d0adb68ac18
L E M O N <ryme.care@hotmail.com>
2024-02-01 22:51:40 +0530

    Update quotes.py

--- plugins/modules/quotes.py
+++ plugins/modules/quotes.py
@@ -1,7 +1,19 @@
-from plugins.modules.urban import get_json
+import aiohttp
 
-async def get_quotes(message):
-  data = await get_json(f"https://api.quotable.io/quotes/random")
-  quotes = data.get('content')
-  await message.reply(f"{quotes}")
-  
+async def get_quotes():
+    url = "https://api.quotable.io/quotes/random"
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            if response.status == 200:
+                quote_data = await response.json()
+                
+                # Check if the response is a list of quotes
+                if isinstance(quote_data, list) and len(quote_data) > 0:
+                    quote = quote_data[0]
+                    return quote.get("content", None)
+                
+                # If not a list, assume it's a single quote
+                return quote_data.get("content", None)
+            else:
+                return None
