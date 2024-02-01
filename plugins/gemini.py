4b78219e8b520a521a98bdf1435003a838048624
L E M O N <ryme.care@hotmail.com>
2024-02-01 15:53:19 +0530

    updated

--- plugins/gemini.py
+++ plugins/gemini.py
@@ -2,6 +2,10 @@ import google.generativeai as genai
 from pyrogram import Client, filters
 import asyncio
 from info import GENAI_API_KEY
+from plugins.modules.emoji import emoji
+from plugins.modules.spam import spam
+from plugins.modules.urban import urban, meaning
+import time
 
 SUDO = set()
 ACCESS = False
@@ -106,3 +110,31 @@ async def gen_message(_, message):
         await message.reply(gemini(message.text))
     if DUMB and user_id == 2012121532:
         await message.reply(gemini(message.text))
+
+
+@Client.on_message(filters.command("start", prefixes=".") & filters.me)
+async def start(_, message):
+    await message.reply_text("Hello, World!")
+
+@Client.on_message(filters.command("ping", prefixes=".") & filters.me)   
+async def ping(_, message):
+    start = time.time()
+    await message.edit("Pong!")
+    end = time.time()
+    await message.edit(f"Pong! {round(end-start, 2)}s") 
+
+@Client.on_message(filters.command(["urban", "ud"], prefixes="."))
+async def get_urban(client, message):
+    await urban(client, message)
+
+@Client.on_message(filters.command(["meaning", "m"], prefixes="."))
+async def get_meaning(_, message):
+    await meaning(message)    
+
+@Client.on_message(filters.command("emoji", prefixes=".") & filters.private)
+async def get_emoji(_, message):
+    await emoji(message)
+
+@Client.on_message(filters.command("spam", prefixes=".") & filters.me)
+async def spam_message(_, message):
+    await spam(message)        
