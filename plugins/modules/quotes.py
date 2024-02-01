7cb4e8f0ad69eed9e81a621765de2660d70fbbe7
L E M O N <ryme.care@hotmail.com>
2024-02-01 22:47:23 +0530

    Update quotes.py

--- plugins/modules/quotes.py
+++ plugins/modules/quotes.py
@@ -2,6 +2,6 @@ from plugins.modules.urban import get_json
 
 async def get_quotes(message):
   data = await get_json(f"https://api.quotable.io/quotes/random")
-  quotes = data.get("content", None)
+  quotes = data.get('content')
   await message.reply(f"{quotes}")
   
