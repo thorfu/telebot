5f94dae1590a4fe52086fe7ab77b5611c9b3c5be
R A Z i <ryme.care@hotmail.com>
2024-05-19 12:31:13 +0530

    updated

---
+++ plugins/accept.py
@@ -0,0 +1,28 @@
+from pyrogram import Client, filters
+from pyrogram.errors import FloodWait
+import asyncio, logging
+
+@Client.on_message(filters.command(["run", "approve"], [".", "/"]))                     
+async def approve(client, message):
+    Id = message.chat.id
+    await message.delete(True)
+ 
+    try:
+       while True:
+           try:
+               await client.approve_all_chat_join_requests(Id)         
+           except FloodWait as t:
+               asyncio.sleep(t.value)
+               await client.approve_all_chat_join_requests(Id) 
+           except Exception as e:
+               logging.error(str(e))
+    except FloodWait as s:
+        asyncio.sleep(s.value)
+        while True:
+           try:
+               await client.approve_all_chat_join_requests(Id)         
+           except FloodWait as t:
+               asyncio.sleep(t.value)
+               await client.approve_all_chat_join_requests(Id) 
+           except Exception as e:
+               logging.error(str(e))
---
+++ plugins/delete.py
@@ -0,0 +1,25 @@
+from pyrogram import Client, filters
+from pyrogram.errors import FloodWait
+import asyncio
+
+# command to delete all messages
+@Client.on_message(filters.me & filters.command("clearchat", prefixes="."))
+async def clearchat(client, message):
+    chat_id = message.chat.id
+
+    # send msg to show that bot is working
+    await message.edit("Deleting all messages...")
+    await asyncio.sleep(2)
+
+    # get all messages
+    async for msg in client.get_chat_history(chat_id):
+        try:
+            # delete message
+            await client.delete_user_history(chat_id, msg.from_user.id)
+        except FloodWait as e:
+            # wait for a while
+            print(e)
+            await asyncio.sleep(e.x)
+        except Exception as e:
+            print(e)
+            pass
