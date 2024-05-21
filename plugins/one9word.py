99cb1b360e12f44b2a2bce64b9af20d13c6bdb7d
R A Z i <ryme.care@hotmail.com>
2024-05-21 16:22:43 +0530

    updated

--- plugins/one9word.py
+++ plugins/one9word.py
@@ -8,6 +8,7 @@ nltk.download('words')
 english_words = set(nltk.corpus.words.words())
 
 ONE9 = False
+used_words = set()  # Store used words here
 starting_letter_pattern = r"start with ([A-Z])"
 min_length_pattern = r"include at least (\d+) letters"
 
@@ -15,6 +16,7 @@ min_length_pattern = r"include at least (\d+) letters"
 @Client.on_message(filters.command("one9", prefixes=".") & filters.me)
 async def one9word(client, message):
     global ONE9
+    global used_words
     msg = message.text.split(None, 1)
     if len(msg) > 1:
         if msg[1] in ["on", "start", "true"]:
@@ -23,11 +25,13 @@ async def one9word(client, message):
         elif msg[1] in ["off", "stop", "false"]:
             await message.reply("One9word plugin disabled")
             ONE9 = False
+            used_words.clear()  # Clear the used words when ONE9 is False
 
 
 @Client.on_message(filters.text)
 async def handle_incoming_message(client, message):
     global ONE9
+    global used_words
     if ONE9:
         trigger_pattern = f"Turn: {TG_NAME}"
         puzzle_text = message.text
@@ -40,13 +44,14 @@ async def handle_incoming_message(client, message):
                 starting_letter = starting_letter_match.group(1)
                 min_length = int(min_length_match.group(1))
 
-                valid_words = (word for word in english_words if word.startswith(starting_letter) and len(word) >= min_length)
+                valid_words = [word for word in english_words if word.startswith(starting_letter) and len(word) >= min_length and word not in used_words]
 
-                try:
-                    random_word = next(valid_words)
+                if valid_words:
+                    random_word = random.choice(valid_words)
+                    used_words.add(random_word)
                     response_message = f"{random_word}"
                     await client.send_message(message.chat.id, response_message)
-                except StopIteration:
+                else:
                     await message.reply("No valid words found for the given criteria.")
             else:
                 await message.reply("Criteria not found in the puzzle text.")
