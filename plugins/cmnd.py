50015e01cdf66c12eedb18ade166a26595ce380b
R A Z i <ryme.care@hotmail.com>
2024-05-21 11:18:11 +0530

    updated

--- plugins/cmnd.py
+++ plugins/cmnd.py
@@ -12,7 +12,7 @@ from plugins.yt_dl import song, vsong
 
 @Client.on_message(filters.command(["help", "h"], prefixes=".") & filters.me)
 async def help_cmd(_, message):
-    await message.edit(
+    await message.edit_text(
         f"**Commands**\n\n"
         "`.ping` - Check the bot's ping\n"
         "`.urban or .ud <word>` - Get the urban dictionary meaning of the word\n"
@@ -21,7 +21,7 @@ async def help_cmd(_, message):
         "`.facts or .f` - Get random facts\n"
         "`.quotes or .q` - Get random quotes\n"
         "`.ask or .a <question>` - Ask a question\n"
-        "`.pfp` - Change your profile picture with random quotes \n"
+        "`.pfp` - Change your profile picture with random quotes\n"
         "`.run` - Accept all JoinRequest Instantly\n"
         "`.clearchat` - Delete all chat message from your group\n"
     )
