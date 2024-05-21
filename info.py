43186ef948e68ec31ed203590c5cfa4aa3a8ddfc
R A Z i <ryme.care@hotmail.com>
2024-05-21 16:13:11 +0530

    updated

--- info.py
+++ info.py
@@ -5,4 +5,4 @@ API_HASH = os.getenv("API_HASH")
 SESSION = os.getenv("SESSION")
 GENAI_API_KEY = os.getenv("GENAI_API_KEY")
 AUTH_USER = os.getenv("AUTH_USER")
-TG_NAME = os.getenv("TG_NAME", "R A Z i")
+TG_NAME = os.getenv("TG_NAME", "R A Z i") # Your Telegram Name
