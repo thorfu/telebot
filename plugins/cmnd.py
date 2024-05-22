875d2e4a098caed04dc92203d047887c31ca53d7
R A Z i <ryme.care@hotmail.com>
2024-05-22 10:48:30 +0530

    update

--- plugins/cmnd.py
+++ plugins/cmnd.py
@@ -192,5 +192,5 @@ async def ily_cmd(client: Client, message: Message):
 
 @Client.on_message(filters.command("heart", prefixes=".") & filters.me)
 async def heart_cmd(client: Client, message: Message):
-    number = message.command[1] if len(message.command) > 1 else 1
+    number = int(message.command[1]) if len(message.command) > 1 else 0
     await heart_fn(number, message)
